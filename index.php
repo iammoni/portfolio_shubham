b5cdc3783d85e3a886cdf4f617179e2d91e44869
Shubham DHANDA <46134480+iammoni@users.noreply.github.com>
2021-02-24 21:46:23 +0530

    deploying app to heroku php as backend

---
+++ index.php
@@ -0,0 +1,213 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width initial-scale=1"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"/>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <title>Shubham Dhanda</title>
+  <style>
+      body {
+          background-color: black;
+      }
+      .container {
+          margin-top: 25px;
+      }
+      .main_name {
+          padding-left: 30%;
+          padding-top: 30px;
+          color: white;
+          font-size: 85px;
+      }
+      .line {
+          margin-top: 100px;
+      }
+       .line hr {    
+        width:80%;
+        height: 3px;
+        margin-left: auto;
+        margin-right: auto;
+       background-color: white;
+      }
+      p {
+          color: darkblue;
+      }
+      .side a {
+          text-align: center;
+      }
+
+      .link_line {
+        width:30%;
+        margin-left:auto;
+        margin-right: auto;
+        background-color: white;
+      }
+    
+      .side{
+          text-align: center;
+      }
+      .main p {
+           margin-right: 15%;
+      } 
+      
+  </style>
+</head>
+<body>
+        <audio autoplay loop="loop"> 
+        <source src="./music/audio.mp3" type="audio/mpeg"/>
+        </audio>
+<div class="container">
+    <img src="./images/circle-cropped.png" style="float:left;width:250px;height:200px;"/>
+    <div>
+        <h1 class="main_name">moni DHANDA</h1>
+    </div>
+    <div class="line">
+        <p style="text-align: center;color:red;"> <i>Talk is cheap show me the code ~Linus Torvalds</i></p>
+        <hr/>
+    </div><!--Line Drawing-->
+</div> <!--End of container div-->
+
+
+<div class="container-fluid row">
+    <div class="col-sm-4 side"  >
+     <h2 style="color: white;margin-bottom: 15px;">Goto</h2>
+     <a href="#about_me" style="color: white;">About Me</a>
+     <hr class="link_line"/>
+     
+     <a href="#internships" style="color: white;">Internships</a>
+     <hr class="link_line"/>
+     
+     <a href="#education" style="color: white;">Education History</a>
+     <hr class="link_line"/>
+
+     
+     <a href="#skills" style="color: white;">Skills</a>
+     <hr class="link_line"/>
+     
+     <a href="#projects" style="color: white;">Projects</a>
+     <hr class="link_line"/>
+
+     
+     <a href="#extra_curicular" style="color: white;">Extra Curricular</a>
+     <hr class="link_line"/>
+     
+     
+     <a href="#certificates" style="color: white;">Certificates</a>
+     <hr class="link_line"/>
+
+     <a href="#Contact_information" style="color: white;">Contact Me</a>
+        
+    </div><!--End of Side-->
+    <div class="col-sm-8 main">
+        <div id="about_me" style="padding-left: 5px;">
+        <h2 style="color: white;">About Me</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and
+             typesetting industry. Lorem Ipsum has been the industry's
+              standard dummy text ever since the 1500s, when an unknown 
+              printer took a galley of type and scrambled it to make a type s
+              pecimen book. It has survived not only five centuries, but also 
+              the leap into electronic typesetting, remaining essentially unchanged.
+               It was popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with
+             desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+       </div> <!--End of About Me-->
+       
+       <div id="internships" style="padding-left: 5px;">
+        <h2 style="color: white;">Internships</h2>
+        <p>Lorem Ipsum is simply dummy text of the 
+            printing and typesetting industry. Lorem Ipsum has been the industry's
+             standard dummy text ever since the 1500s, when an unknown
+              printer took a galley of type and scrambled it to make a type specimen book.
+               It has survived not only five centuries, but also the leap into 
+               electronic typesetting, remaining essentially unchanged. It
+                was popularised in the 1960s with the release of Letraset s
+                heets containing Lorem Ipsum passages, and more recently with desktop publishing
+             software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+       </div> <!--End of Skills-->
+
+       <div id="education" style="padding-left: 5px;">
+        <h2 style="color: white;">Education History</h2>
+        <p>Lorem Ipsum is simply dummy text of the 
+            printing and typesetting industry. Lorem Ipsum has been the industry's
+             standard dummy text ever since the 1500s, when an unknown
+              printer took a galley of type and scrambled it to make a type specimen book.
+               It has survived not only five centuries, but also the leap into 
+               electronic typesetting, remaining essentially unchanged. It
+                was popularised in the 1960s with the release of Letraset s
+                heets containing Lorem Ipsum passages, and more recently with desktop publishing
+             software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+       </div> <!--End of Education-->
+
+       <div id="skills" style="padding-left: 5px;">
+        <h2 style="color: white;">Technical Skills</h2>
+        <p>Lorem Ipsum is simply dummy text of the 
+            printing and typesetting industry. Lorem Ipsum has been the industry's
+             standard dummy text ever since the 1500s, when an unknown
+              printer took a galley of type and scrambled it to make a type specimen book.
+               It has survived not only five centuries, but also the leap into 
+               electronic typesetting, remaining essentially unchanged. It
+                was popularised in the 1960s with the release of Letraset s
+                heets containing Lorem Ipsum passages, and more recently with desktop publishing
+             software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+       </div> <!--End of Skills-->
+      
+       <div id="projects" style="padding-left: 5px;">
+        <h2 style="color: white;">Projects</h2>
+        <p>Lorem Ipsum is simply dummy text of the 
+            printing and typesetting industry. Lorem Ipsum has been the industry's
+             standard dummy text ever since the 1500s, when an unknown
+              printer took a galley of type and scrambled it to make a type specimen book.
+               It has survived not only five centuries, but also the leap into 
+               electronic typesetting, remaining essentially unchanged. It
+                was popularised in the 1960s with the release of Letraset s
+                heets containing Lorem Ipsum passages, and more recently with desktop publishing
+             software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+       </div> <!--End of About Me-->
+
+       <div id="extra_curicular" style="padding-left: 5px;">
+        <h2 style="color: white;">Extra</h2>
+        <p>Lorem Ipsum is simply dummy text of the 
+            printing and typesetting industry. Lorem Ipsum has been the industry's
+             standard dummy text ever since the 1500s, when an unknown
+              printer took a galley of type and scrambled it to make a type specimen book.
+               It has survived not only five centuries, but also the leap into 
+               electronic typesetting, remaining essentially unchanged. It
+                was popularised in the 1960s with the release of Letraset s
+                heets containing Lorem Ipsum passages, and more recently with desktop publishing
+             software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+       </div> <!--End of Contact_Infomations-->
+       
+       <div id="certificates" style="padding-left: 5px;">
+        <h2 style="color: white;">Cetificates</h2>
+        <p>Lorem Ipsum is simply dummy text of the 
+            printing and typesetting industry. Lorem Ipsum has been the industry's
+             standard dummy text ever since the 1500s, when an unknown
+              printer took a galley of type and scrambled it to make a type specimen book.
+               It has survived not only five centuries, but also the leap into 
+               electronic typesetting, remaining essentially unchanged. It
+                was popularised in the 1960s with the release of Letraset s
+                heets containing Lorem Ipsum passages, and more recently with desktop publishing
+             software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+       </div> <!--End of Skills-->
+
+       <div id="Contact_information" style="padding-left: 5px;">
+        <h2 style="color: white;">Contact Me</h2>
+        <p>Lorem Ipsum is simply dummy text of the 
+            printing and typesetting industry. Lorem Ipsum has been the industry's
+             standard dummy text ever since the 1500s, when an unknown
+              printer took a galley of type and scrambled it to make a type specimen book.
+               It has survived not only five centuries, but also the leap into 
+               electronic typesetting, remaining essentially unchanged. It
+                was popularised in the 1960s with the release of Letraset s
+                heets containing Lorem Ipsum passages, and more recently with desktop publishing
+             software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+       </div> <!--End of Contact_Infomations-->
+
+    </div><!--End of main -->
+</div> <!--End of conainer-fluid-->
+
+</body>
+
+</html>
